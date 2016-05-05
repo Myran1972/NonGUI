c6cdd6a569006e244e7b1fa17a161e69ae2a9607
Myran1972 <myran1972@hotmail.com>
2016-05-05 12:55:05 +0200

    Missade test och Exceptionsklass

---
+++ src/Main/ForeignKeyException.java
@@ -0,0 +1,12 @@
+package Main;
+
+public class ForeignKeyException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ForeignKeyException(){
+		super("FOREIGN KEY constraint failed");
+	}
+}
